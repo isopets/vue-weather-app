d8297a0dea0836465670c94f516d9175d0a1d75d
yuto isogai <jandp.0717@gmail.com>
2022-07-14 15:26:43 +0900

    add css

---
+++ .history/src/App_20220714152632.vue
@@ -0,0 +1,33 @@
+<template>
+  <Title /><Form @submit-form="getWeather" /><Results :results="results" />
+</template>
+
+<script setup>
+import {reactive} from "vue";
+import axios from "axios";
+import Title from "./components/Title.vue";
+import Form from "./components/Form.vue";
+import Results from "./components/Results.vue";
+
+const results = reactive({
+  country: "",
+  cityName: "",
+  temperature: "",
+  conditionText: "",
+  icon: "",
+});
+
+const getWeather = city => {
+  axios
+    .get(
+      `http://api.weatherapi.com/v1/current.json?key=75a9ebd02b7d4a81b1a40652221407&q=${city}&aqi=no`
+    )
+    .then(res => {
+      (results.country = res.data.location.country),
+        (results.cityName = res.data.location.name),
+        (results.temperature = res.data.current.temp_c),
+        (results.conditionText = res.data.current.condition.text),
+        (results.icon = res.data.current.condition.icon);
+    });
+};
+</script>
